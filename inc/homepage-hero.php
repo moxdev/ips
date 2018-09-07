6bd88dfc599c1694dfd4bcaa294e00192bf4f4ae
moxdev <moxdev1@gmail.com>
2018-09-07 16:54:49 -0400

    styles for mobile hero

--- inc/homepage-hero.php
+++ inc/homepage-hero.php
@@ -9,16 +9,33 @@
   function ips_home_hero() {
     if( function_exists( 'get_field' ) ) {
       $hero = get_field( 'home_hero' ); ?>
+
         <section class='hero'>
+
           <img src="<?php echo esc_url( $hero['background_image']['sizes']['home-hero'] ); ?>" alt="<?php echo esc_attr( $hero['background_image']['alt'] ); ?>" description="<?php echo esc_attr( $hero['background_image']['description'] ); ?>">
+
           <div class='services-container'>
-            <?php echo $hero['services_list']; ?>
+            <span></span>
+            <div class='services-list'>
+              <?php echo $hero['services_list']; ?>
+            </div>
           </div>
+
           <div class='callout-container'>
-            <div class='title'><?php echo esc_html( $hero['title'] ); ?></div>
-            <div class='sub-title'><?php echo esc_html( $hero['subtitle'] ); ?></div>
-            <a class="btn" href='<?php echo $hero['button_page_link']; ?>'><?php echo esc_html( $hero['button_text'] ); ?></a>
+
+            <div class='title-container'>
+              <div class='title'><?php echo esc_html( $hero['title'] ); ?></div>
+
+              <div class='subtitle'><?php echo esc_html( $hero['subtitle'] ); ?></div>
+            </div>
+
+            <div class='button-container'>
+              <a class="btn" href='<?php echo $hero['button_page_link']; ?>'><?php echo esc_html( $hero['button_text'] ); ?></a>
+              <span></span>
+            </div>
+
           </div>
+
         </section>
       <?php
     }
