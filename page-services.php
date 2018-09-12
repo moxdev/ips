f9be210c240037d850561b76170246991688eabf
moxdev <moxdev1@gmail.com>
2018-09-12 15:53:05 -0400

    added inner-wrapper for flex of main

--- page-services.php
+++ page-services.php
@@ -10,22 +10,26 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<hr class='decoration-header'>
-			<div class='wrapper'>
+			<span class='decoration-header'></span>
+			<div class='main-wrapper'>
+				<div class='main-inner-wrapper'>
 
-				<?php
-				while ( have_posts() ) :
-					the_post();
+					<?php
+					while ( have_posts() ) :
+						the_post();
 
-					get_template_part( 'template-parts/content', 'page' );
+						get_template_part( 'template-parts/content', 'page' );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
-				endwhile; // End of the loop.
-				?>
+					endwhile; // End of the loop.
+					?>
+
+
+				</div>
 
 			</div>
 
